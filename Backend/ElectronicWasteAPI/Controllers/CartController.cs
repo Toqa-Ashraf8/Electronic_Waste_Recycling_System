60ee18fe8f7bce15cd3c344967df5accd191755d
Toqa Ashraf <toqaaashraf87@gmail.com>
2026-04-19 15:52:00 +0200

    refactor CartController to use Entity Framework Core instead of ADO.NET

--- Backend/ElectronicWasteAPI/Controllers/CartController.cs
+++ Backend/ElectronicWasteAPI/Controllers/CartController.cs
@@ -15,12 +15,10 @@ namespace ElectronicWasteAPI.Controllers
     {
         private readonly DataContext _context;
         private readonly IWebHostEnvironment _env;
-        SqlConnection conn;
         public CartController(DataContext context, IWebHostEnvironment env)
         {
             _context = context;
             _env = env;
-            conn = new SqlConnection(_context.Database.GetConnectionString());
         }
         [Route("UploadProductImage")]
         [HttpPost]
@@ -44,159 +42,79 @@ namespace ElectronicWasteAPI.Controllers
             bool saved = false;
             bool updated = false;
             int id = Convert.ToInt32(cat.CategoryID);
-            if (conn.State == ConnectionState.Closed) conn.Open();
-            using(SqlTransaction transaction = conn.BeginTransaction())
-            {
                 try
                 {
-                    if (id == 0)
+                    var existingCat = _context.CartCategories
+                                  .Include(c => c.products) 
+                                  .FirstOrDefault(c => c.CategoryID == id);
+                    if (id == 0 || existingCat==null)
                     {
-                        string sqli = @"insert into CartCategories (CategoryName) 
-                                        values(@CategoryName)select SCOPE_IDENTITY()";
-
-                        using(SqlCommand cmd=new SqlCommand(sqli, conn, transaction))
-                        {
-                            cmd.Parameters.Clear();
-                            cmd.Parameters.AddWithValue("@CategoryName", cat.CategoryName);
-                            id = Convert.ToInt32(cmd.ExecuteScalar());
-                            saved = true;
-                        }
-
+                                _context.CartCategories.Add(cat); 
+                                saved = true;
                     }
                     else
                     {
-                        string sqlu = @"Update CartCategories set CategoryName=@CategoryName
-                                        where CategoryID=@CategoryID"; 
-
-
-                        using (SqlCommand cmd = new SqlCommand(sqlu, conn, transaction))
-                        {
-                            cmd.Parameters.Clear();
-                            cmd.Parameters.AddWithValue("@CategoryName", cat.CategoryName);
-                            cmd.Parameters.AddWithValue("@CategoryID", id);
-                            cmd.ExecuteNonQuery();
+                            existingCat.CategoryName = cat.CategoryName;
+                            _context.Products.RemoveRange(existingCat.products);
+                            existingCat.products = cat.products;
                             updated = true;
-                        }
+                        
 
                     }
-                    if(cat.products.Count>0 || cat.products != null)
-                    {
-                        string deleteP = @"delete Products where CategoryID=@CategoryID";
-                        using (SqlCommand cmd = new SqlCommand(deleteP, conn, transaction))
-                        {
-                            cmd.Parameters.Clear();
-                            cmd.Parameters.AddWithValue("@CategoryID", id);
-                            cmd.ExecuteNonQuery(); 
-                        }
-                        string insertP = @"insert into Products 
-                                        (serial,ProductName,ProductPrice,Stock,Points,ProductImagePath,Description,CategoryID)
-                                        values(@serial,@ProductName,@ProductPrice,@Stock,@Points,
-                                        @ProductImagePath,@Description,@CategoryID)select SCOPE_IDENTITY()";
-                        foreach (var product in cat.products)
-                        {
-                            using (SqlCommand cmd = new SqlCommand(insertP, conn, transaction))
-                            {
-                                cmd.Parameters.Clear();
-                                cmd.Parameters.AddWithValue("@serial", product.serial);
-                                cmd.Parameters.AddWithValue("@ProductName", product.ProductName);
-                                cmd.Parameters.AddWithValue("@ProductPrice", product.ProductPrice);
-                                cmd.Parameters.AddWithValue("@Stock", product.Stock);
-                                cmd.Parameters.AddWithValue("@Points", product.Points);
-                                cmd.Parameters.AddWithValue("@ProductImagePath", product.ProductImagePath);
-                                cmd.Parameters.AddWithValue("@Description", product.Description);
-                                cmd.Parameters.AddWithValue("@CategoryID",id);
-                                cmd.ExecuteScalar();     
-                            }
-
-                        }
-
-                    }
-                    transaction.Commit();
+                    _context.SaveChanges();
                     return Ok(new { saved = saved, id = id ,updated=updated});
                 }
                 catch (Exception ex)
                 {
-                    transaction.Rollback();
-                    return BadRequest(new { error = ex.Message, saved = false });
+                   
+                    return BadRequest(new { error = ex.Message, saved = false ,updated=false});
                 }
-                finally
-                {
-                    if (conn.State == ConnectionState.Open) conn.Close();
-                }
-
-
-            }
-
+              
         }
 
         [Route("GetCategories")]
         [HttpGet]
-        public IActionResult GetCategories()
+        public async Task<IActionResult> GetCategories()
         {
-            DataTable dt = new DataTable();
-            string sqlg = "select * from CartCategories";
-            SqlDataAdapter da = new SqlDataAdapter(sqlg, conn);
-            da.Fill(dt);
-            return Ok(dt);
+            var categories = await _context.CartCategories.ToListAsync();
+            return Ok(categories);
         }
         [Route("GetProductsByCat")]
         [HttpPost]
-        public IActionResult GetProductsByCat(int catId)
+        public async Task<IActionResult> GetProductsByCat(int catId)
         {
 
-            DataTable dt = new DataTable();
-            string sqlg = "select * from Products where CategoryID=@CategoryID";
-            using(SqlCommand cmd=new SqlCommand(sqlg, conn))
-            {
-                cmd.Parameters.AddWithValue("@CategoryID", catId);
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                da.Fill(dt);
-            }
-          
-            return Ok(dt);
+            var products = await _context.Products
+                           .Where(p => p.CategoryID == catId)
+                           .ToListAsync();
+            return Ok(products);
 
         }
         [Route("DeleteCatWithProducts")]
         [HttpDelete]
-        public IActionResult DeleteCatWithProducts(int catId)
+        public async Task<IActionResult> DeleteCatWithProducts(int catId)
         {
             bool deleted = false;
-            if (conn.State == ConnectionState.Closed) conn.Open();
-            using(SqlTransaction transaction = conn.BeginTransaction())
-            {
-                try
-                {
-                    if (catId > 0 || catId!=0)
+              try
+              {
+                    if (catId > 0 || catId != 0)
                     {
-                        string deleteP = "delete Products where CategoryID=@CategoryID";
-                        using (SqlCommand cmd = new SqlCommand(deleteP, conn,transaction))
-                        {
-                            cmd.Parameters.AddWithValue("@CategoryID", catId);
-                            cmd.ExecuteNonQuery();
-                        }
-                        string deleteC = "delete CartCategories where CategoryID=@CategoryID";
-                        using (SqlCommand cmd = new SqlCommand(deleteC, conn,transaction))
-                        {
-                            cmd.Parameters.AddWithValue("@CategoryID", catId);
-                            cmd.ExecuteNonQuery();
-                            deleted = true;
-                        }
-                    }
-                    transaction.Commit();
-                    return Ok(new { deleted = true });
-                }
-                catch (Exception ex)
-                {
-                    transaction.Rollback();
-                    return BadRequest(new {error=ex.Message,deleted=false});
-                }
-                finally
-                {
-                    if (conn.State == ConnectionState.Open) conn.Close();
-                }
-            }
-           
+                        var category = await _context.CartCategories
+                                   .Include(c => c.products)
+                                   .FirstOrDefaultAsync(c => c.CategoryID == catId);
 
+                        _context.Products.RemoveRange(category.products);
+                        _context.CartCategories.Remove(category);
+                    }
+                    await _context.SaveChangesAsync();
+                    return Ok(new { deleted = true });
+              }
+              catch (Exception ex)
+              {
+                  return BadRequest(new {error=ex.Message,deleted=false});
+              }
+                
+           
         }
     }
 }
